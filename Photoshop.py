98ae109caa48579d236ff08e64781985480dc4a0
Anonymous <martynnazar11@gmail.com>
2024-10-20 16:23:00 +0300

    rrrrrjfn x

--- Photoshop.py
+++ Photoshop.py
@@ -1,4 +1,39 @@
+import os
+from PIL import Image.Qt
+from PIL.ImageQt import QImage
+from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import *
+
+def pil2pixmap(im):
+    if im.mode == "RGB":
+        r, g, b = im.split()
+        im = Image.merge("RGB", (b, g, r))
+    elif im.mode == "RGBA":
+        r, g, b, a = im.split()
+        im = Image.merge("RGBA", (b, g, r, a))
+    elif im.mode == "L":
+        im = im.convert("RGBA")
+    im2 = im.convert("RGBA")
+    data = im2.tobytes("raw", "RGBA")
+    qim = QImage(data, im.size[0], im.size[1], QImage.Format_ARGB32)
+    pixmap = QPixmap.fromImage(qim)
+    return pixmap
+
+class PhotoManager:
+    def  __init__(self):
+        self.photo = None
+        self.dir_btn = None
+        self.filename = None
+
+    def load(self):
+        image_path = os.path.join(self.folder, self.filename)
+        self.photo = Image.open(image_path)
+
+    def show_images(self, image_lbl):
+        pixels = pil2pixmap(self.photo)
+        pixels = pixels.scaledToWidth(500)
+        image_lbl.setPixmap(pixels)
+
 
 
 app = QApplication([])
@@ -58,7 +93,29 @@ h1.addWidget(filter4_btn)
 h1.addWidget(filter5_btn)
 v2.addLayout(h1)
 
+photo_manager = PhotoManager()
 
+def open_folder():
+    photo_manager.folder = QFileDialog.getExistingDirectory()
+    files = os.listdir(photo_manager.folder)
+    images_list.clear()
+
+
+    #перебирати файли
+        #добавляти з потрібним розширенням (endwith)
+    images_list.addItems(files)
+
+def show_chosen_image():
+    photo_manager.filename = images_list.currentItem().text()
+    photo_manager.load()
+    photo_manager.show_images(image_lbl)
+
+
+images_list.currentRowChanged.connect(show_chosen_image)
+dir_btn.clicked.connect(open_folder)
 window.setLayout(mainline)
 window.show()
 app.exec()
+
+
+
